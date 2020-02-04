d11a5252aeba8cf2d1ae2925a8178f40d8c5a6cb
Joao Antonio <joaopaka@outlook.de>
2020-02-04 17:15:48 +0100

    Deletet dijkstra2

--- src/com/company/CalculatingOfNodes.java
+++ src/com/company/CalculatingOfNodes.java
@@ -36,37 +36,6 @@ public class CalculatingOfNodes{
 		}
 	}
 
-	protected static void dijkstra(List<NodeGraph> nodes, List<EdgeGraph> edge,  Integer numbOfNodes, Integer numbOfEdges, Integer firstNode, Integer secondNode) {
-		nodes.get(firstNode).setWeight(0);
-		nodes.get(firstNode).setPreNode(null);
-		nodes.get(firstNode).setReachable(true);
-
-		for (int i = 0; i < numbOfNodes; i++) {
-			int keyId = newKey(nodes, numbOfNodes);
-			for (int j = 0; j < numbOfEdges; j++) {
-				int sourceId = edge.get(j).getSourceId();
-				int targetId = edge.get(j).getTargetId();
-				if (nodes.get(keyId).getNodeId() == targetId) {
-					if (nodes.get(keyId).getWeight() + edge.get(j).getWeight() < nodes.get(sourceId).getWeight()
-							|| nodes.get(sourceId).isReachable() == false) {
-						nodes.get(sourceId).setWeight(nodes.get(keyId).getWeight() + edge.get(j).getWeight());
-						nodes.get(sourceId).setPreNode(nodes.get(keyId).getNodeId());
-						nodes.get(sourceId).setReachable(true);
-					}
-				}
-				if (nodes.get(keyId).getNodeId() == sourceId) {
-					if (nodes.get(keyId).getWeight() + edge.get(j).getWeight() < nodes.get(targetId).getWeight()
-							|| nodes.get(targetId).isReachable() == false) {
-						nodes.get(targetId).setWeight(nodes.get(keyId).getWeight() + edge.get(j).getWeight());
-						nodes.get(targetId).setPreNode(nodes.get(keyId).getNodeId());
-						nodes.get(targetId).setReachable(true);
-					}
-				}
-			}
-			nodes.get(keyId).setUnused(false);
-		}
-	}
-
 	private static int newKey(List<NodeGraph> nodes, Integer numbOfNodes) {
 		int lowestWeight = Integer.MAX_VALUE;
 		int keyId = 0;
